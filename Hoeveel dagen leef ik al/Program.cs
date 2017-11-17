86e94be2ffc01c65a396f923e62205ba02cacd9a
Zegels Nick <nick.zegels@immalle.eu>
2017-11-17 10:33:22 +0100

    Add project files.

---
+++ Hoeveel dagen leef ik al/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hoeveel_dagen_leef_ik_al
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
